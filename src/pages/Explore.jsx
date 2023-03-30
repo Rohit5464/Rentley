b46053c99acfdd6fe2be2a59619a103e62749a51
Rohit5464 <rohit.work5464@gmail.com>
2023-03-30 23:17:25 +0530

    final change

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -17,11 +17,11 @@ const Explore = () => {
             <div className="exploreCategories">     
                 <Link to='/category/rent'>
                     <img src={rentCategoryImage} alt="rent" className='exploreCategoryImg' />
-                    <p className="exploreCategoryName">Places for rent</p>
+                    <p className="exploreCategoryName">Products for rent</p>
                 </Link>
                 <Link to='/category/sell'>
                     <img src={sellCategoryImage} alt="sell" className='exploreCategoryImg' />
-                    <p className="exploreCategoryName">Places for sale</p>
+                    <p className="exploreCategoryName">Products for sale</p>
                 </Link>
                 
             </div>
